ddcda328f178b22424a8e6b69d057c3dae78f273
Vinam Arora <Vinam Arora>
2016-05-05 15:35:07 +0530

    Made some changes. Obvious ones :P

--- semic_diffusion.m
+++ semic_diffusion.m
@@ -1,4 +1,6 @@
-tmax = 10^-10;
+tmax = 10^-14;
+delX = 5*10^-10;
+rangeX = 5*10^-7;
 
 if tmax > 10^-13
     delT = 10^-16;
@@ -6,8 +8,6 @@ else
     delT = tmax/1000;
 end
 
-delX = 10*10^-10;
-rangeX = 5*10^-6;
 Dp = 1.2432*10^-3;
 
 % for tmax = 10^-15s : delX = 5*10^-10, rangeX = 5*10^-8
@@ -15,8 +15,8 @@ Dp = 1.2432*10^-3;
 % for tmax = 10^-13s : delX = 5*10^-10, rangeX = 5*10^-7
 % for tmax = 10^-12s : delX = 5*10^-10, rangeX = 5*10^-6
 % for tmax = 10^-11s : delX = 5*10^-10, rangeX = 5*10^-6
-% for tmax = 10^-10s : delX = 10*10^-10, rangeX = 5*10^-5
-% for tmax = 10^-09s : delX = 10*10^-10, rangeX = 5*10^-5
+% for tmax = 10^-10s : delX = 5*10^-10, rangeX = 5*10^-5
+% for tmax = 10^-09s : delX = 10*10^-10, rangeX = 5*10^-4
 % for tmax = 10^-08s : delX = 10*10^-10, rangeX = 5*10^-4
 
 elements = int16(rangeX/delX);
@@ -24,7 +24,7 @@ p = zeros(1, 2 + elements);
 p(int16(elements/2)) = 10^20;
 p(1 + int16(elements/2)) = 10^20;
 temp = zeros(1, 2 + elements);
-x = [1:1:2 + elements];
+x = [-int16(elements/2):1:1+int16(elements/2)];
 
 iterations = tmax/delT;
 for t = 0:iterations
